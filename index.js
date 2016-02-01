100adf7f39ac4b6bcd8f8cadd8092bdbfaa2d76c
Jeremy <jermspeaks@gmail.com>
2016-01-31 20:59:56 -0800

    mongo

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // Retrieve
 var MongoClient = require('mongodb').MongoClient;
 
+var url = 'mongodb://localhost:27017/movie';
+
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
+MongoClient.connect(url, function(err, db) {
+  // Error case
+  if (err) {
+    return console.dir(err);
   }
+
+  var collection = db.collection('watched');
+
+  collection.update({genres: /,/}, update, {w:1, multi: true}, function(err, results) {
+    console.log(results);
+  });
+
+  db.close();
 });
